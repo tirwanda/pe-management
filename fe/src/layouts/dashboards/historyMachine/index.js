ea9940a2be9d4bc80e08c0fc3b6b71edd044fd08
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-02-22 22:23:58 +0700

    initial commit

---
+++ fe/src/layouts/dashboards/historyMachine/index.js
@@ -0,0 +1,121 @@
+// import { useState } from "react";
+// @mui material components
+import Grid from "@mui/material/Grid";
+import Tooltip from "@mui/material/Tooltip";
+import Icon from "@mui/material/Icon";
+import Card from "@mui/material/Card";
+
+// Material Dashboard 2 PRO React components
+import MDBox from "components/MDBox";
+import MDBadgeDot from "components/MDBadgeDot";
+import MDButton from "components/MDButton";
+import MDTypography from "components/MDTypography";
+
+// Material Dashboard 2 PRO React examples
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import DefaultStatisticsCard from "examples/Cards/StatisticsCards/DefaultStatisticsCard";
+import DefaultLineChart from "examples/Charts/LineCharts/DefaultLineChart";
+import HorizontalBarChart from "examples/Charts/BarCharts/HorizontalBarChart";
+import SalesTable from "examples/Tables/SalesTable";
+import DataTable from "examples/Tables/DataTable";
+
+// Sales dashboard components
+import ChannelsChart from "layouts/dashboards/historyMachine/components/ChannelsChart";
+
+// Data
+import defaultLineChartData from "layouts/dashboards/historyMachine/data/defaultLineChartData";
+import horizontalBarChartData from "layouts/dashboards/historyMachine/data/horizontalBarChartData";
+import salesTableData from "layouts/dashboards/historyMachine/data/salesTableData";
+import dataTableData from "layouts/dashboards/historyMachine/data/dataTableData";
+
+function Sales() {
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox py={3}>
+        <MDBox mb={3}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={4}>
+              <DefaultStatisticsCard title="Total Line" count="3 Lines" />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <DefaultStatisticsCard title="Total Aset" count="94" />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <DefaultStatisticsCard title="Model" count="7" />
+            </Grid>
+          </Grid>
+        </MDBox>
+        <MDBox mb={3}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6} lg={4}>
+              <ChannelsChart />
+            </Grid>
+            <Grid item xs={12} sm={6} lg={8}>
+              <DefaultLineChart
+                title="Revenue"
+                description={
+                  <MDBox display="flex" justifyContent="space-between">
+                    <MDBox display="flex" ml={-1}>
+                      <MDBadgeDot color="info" size="sm" badgeContent="Facebook Ads" />
+                      <MDBadgeDot color="dark" size="sm" badgeContent="Google Ads" />
+                    </MDBox>
+                    <MDBox mt={-4} mr={-1} position="absolute" right="1.5rem">
+                      <Tooltip title="See which ads perform better" placement="left" arrow>
+                        <MDButton
+                          variant="outlined"
+                          color="secondary"
+                          size="small"
+                          circular
+                          iconOnly
+                        >
+                          <Icon>priority_high</Icon>
+                        </MDButton>
+                      </Tooltip>
+                    </MDBox>
+                  </MDBox>
+                }
+                chart={defaultLineChartData}
+              />
+            </Grid>
+          </Grid>
+        </MDBox>
+        <MDBox mb={3}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} lg={8}>
+              <HorizontalBarChart title="Sales by age" chart={horizontalBarChartData} />
+            </Grid>
+            <Grid item xs={12} lg={4}>
+              <SalesTable title="Sales by Country" rows={salesTableData} />
+            </Grid>
+          </Grid>
+        </MDBox>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox pt={3} px={3}>
+                <MDTypography variant="h6" fontWeight="medium">
+                  Top Selling Products
+                </MDTypography>
+              </MDBox>
+              <MDBox py={1}>
+                <DataTable
+                  table={dataTableData}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  isSorted={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+        </Grid>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Sales;
