994738a2c03db370d3961d3f024b792129a13b04
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-02-27 22:19:01 +0700

    Create User and Role Repository

---
+++ be/src/main/java/com/tirwanda/be/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.tirwanda.be.repository;
+
+import com.tirwanda.be.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
