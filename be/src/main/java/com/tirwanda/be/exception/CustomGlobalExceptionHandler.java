148b3e673435a213bb1f2e3618f2e6ff929fcf89
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-03-13 21:11:41 +0700

    Handle ResourceNotFoundException

--- be/src/main/java/com/tirwanda/be/exception/CustomGlobalExceptionHandler.java
+++ be/src/main/java/com/tirwanda/be/exception/CustomGlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -38,5 +39,13 @@ public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler
                 ex.getMessage());
 
         return new ResponseEntity<>(customErrorDetails, HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        CustomErrorDetails customErrorDetails = new CustomErrorDetails(new Date(),
+                ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(customErrorDetails, HttpStatus.NOT_FOUND);
     }
 }
