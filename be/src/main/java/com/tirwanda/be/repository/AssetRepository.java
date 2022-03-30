c21ecc8f102b2afd37326c88e75ef88d02e6bb0f
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-03-30 13:26:21 +0700

    Create Asset Repository

--- be/src/main/java/com/tirwanda/be/repository/AssetRepository.java
+++ be/src/main/java/com/tirwanda/be/repository/AssetRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AssetRepository extends JpaRepository<Asset, Long> {
+    Asset findAssetByAssetNumber(String assetNumber);
 }
