a0c1b3332493dce786f58be1a616b8025cb40da5
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-04-10 21:43:33 +0700

    Create Print Part Service

---
+++ be/src/main/java/com/tirwanda/be/service/part/PrintPartService.java
@@ -0,0 +1,12 @@
+package com.tirwanda.be.service.part;
+
+import com.tirwanda.be.entity.Part;
+import com.tirwanda.be.exception.ResourceNotFoundException;
+
+import java.util.List;
+
+public interface PrintPartService {
+    Part getPartByPartNumber(String partNumber);
+    List<Part> getPartByAssetNumber(String assetNumber) throws ResourceNotFoundException;
+    List<Part> getAllPart();
+}
---
+++ be/src/main/java/com/tirwanda/be/service/part/PrintPartServiceImpl.java
@@ -0,0 +1,42 @@
+package com.tirwanda.be.service.part;
+
+import com.tirwanda.be.entity.Asset;
+import com.tirwanda.be.entity.Part;
+import com.tirwanda.be.exception.ResourceNotFoundException;
+import com.tirwanda.be.repository.AssetRepository;
+import com.tirwanda.be.repository.PartRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@Slf4j
+public class PrintPartServiceImpl implements PrintPartService {
+
+    private final PartRepository partRepository;
+    private final AssetRepository assetRepository;
+
+    @Override
+    public Part getPartByPartNumber(String partNumber) {
+        return partRepository.findPartByPartNumber(partNumber);
+    }
+
+    @Override
+    public List<Part> getPartByAssetNumber(String assetNumber) throws ResourceNotFoundException {
+        Asset asset = assetRepository.findAssetByAssetNumber(assetNumber);
+        if (asset == null) {
+            throw new ResourceNotFoundException("Asset with asset number: " + assetNumber + " is not found");
+        }
+        return asset.getPartList();
+    }
+
+    @Override
+    public List<Part> getAllPart() {
+        return partRepository.findAll();
+    }
+}
