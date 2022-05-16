3ca6bf7c87d4b889f538aad4056fb4e4eb2e7386
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-05-16 13:24:58 +0700

    Create Downtime Repository

---
+++ be/src/main/java/com/tirwanda/be/repository/DowntimeRepository.java
@@ -0,0 +1,13 @@
+package com.tirwanda.be.repository;
+
+import com.tirwanda.be.entity.Downtime;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DowntimeRepository extends JpaRepository<Downtime, Long> {
+    List<Downtime> findDowntimeByWorkOrderContaining(String workOrder);
+    List<Downtime> findDowntimeByStatus(String status);
+}
