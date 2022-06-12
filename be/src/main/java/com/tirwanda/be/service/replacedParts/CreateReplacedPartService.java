c5a1df0b71427a9cf3eb862d6d52268abe876c06
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2022-06-12 21:08:44 +0700

    Modify Create Replaced Part

--- be/src/main/java/com/tirwanda/be/service/replacedParts/CreateReplacedPartService.java
+++ be/src/main/java/com/tirwanda/be/service/replacedParts/CreateReplacedPartService.java
@@ -1,7 +1,8 @@
 package com.tirwanda.be.service.replacedParts;
 
 import com.tirwanda.be.entity.ReplacedParts;
+import com.tirwanda.be.exception.ResourceNotFoundException;
 
 public interface CreateReplacedPartService {
-    ReplacedParts saveReplacedParts(ReplacedParts replacedParts);
+    ReplacedParts saveReplacedParts(ReplacedParts replacedParts) throws ResourceNotFoundException;
 }
--- be/src/main/java/com/tirwanda/be/service/replacedParts/CreateReplacedPartsServiceImpl.java
+++ be/src/main/java/com/tirwanda/be/service/replacedParts/CreateReplacedPartsServiceImpl.java
@@ -1,6 +1,9 @@
 package com.tirwanda.be.service.replacedParts;
 
+import com.tirwanda.be.entity.Part;
 import com.tirwanda.be.entity.ReplacedParts;
+import com.tirwanda.be.exception.ResourceNotFoundException;
+import com.tirwanda.be.repository.PartRepository;
 import com.tirwanda.be.repository.ReplacedPartsRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +18,16 @@ import javax.transaction.Transactional;
 public class CreateReplacedPartsServiceImpl implements CreateReplacedPartService {
 
     private final ReplacedPartsRepository replacedPartsRepository;
+    private final PartRepository partRepository;
 
     @Override
-    public ReplacedParts saveReplacedParts(ReplacedParts replacedParts) {
+    public ReplacedParts saveReplacedParts(ReplacedParts replacedParts) throws ResourceNotFoundException {
+        Part part = partRepository.findPartByPartNumber(replacedParts.getPartNumber());
+
+        if (part == null) throw new ResourceNotFoundException("Part is doesn't exist");
+
+        part.setStock(Math.max(part.getStock() - replacedParts.getQuantity(), 0));
+        partRepository.save(part);
         return replacedPartsRepository.save(replacedParts);
     }
 }
